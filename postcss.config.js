46d1184f29ee9d82bbc7c9cc529d4262442d60f8
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-08-31 12:04:18 +0200

    Fix open source descriptions

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,13 @@ const production = process.env.HUGO_ENV === "production";
 module.exports = {
   plugins: [
     require("tailwindcss"),
+    require("autoprefixer"),
     production &&
       require("@fullhuman/postcss-purgecss")({
         content: ["./layouts/**/*.html", "./content/**/*.html"],
         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
         whitelistPatternsChildren: [/markup/, /pagination/]
       }),
-    production && require("autoprefixer"),
     production && require("cssnano")
   ]
 };
