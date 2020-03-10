eee2488d3aabbbad75afe60583fe2d262d3c592a
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-03-10 17:30:55 +0100

    Goodnight Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,6 @@ module.exports = {
       colors: {
         inherit: "inherit"
       },
-      fontFamily: {
-        sans: [
-          "Inter",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          '"Segoe UI"',
-          "Roboto",
-          '"Helvetica Neue"',
-          "Arial",
-          '"Noto Sans"',
-          "sans-serif",
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"'
-        ]
-      },
       screens: {
         dark: { raw: "(prefers-color-scheme: dark)" }
       }
