c16820320e06895d5f3c3c5b393ede59ab775507
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-03-26 15:24:07 +0100

    Clean up webmentions script

--- webmentions.js
+++ webmentions.js
@@ -1,54 +1,47 @@
 const fs = require("fs");
 const https = require("https");
 
-const postSlugs = fs.readdirSync(`${__dirname}/content/posts`).flatMap(path => {
-  const fullPath = `${__dirname}/content/posts/${path}`;
+fetchWebmentions().then(webmentions => {
+  webmentions.forEach(webmention => {
+    // Sluggify the webmention target URL.
+    // Example: "https://sebastiandedeyne/unix-things/listing-directories/"
+    //   -> "unix-things--listing-directories"
+    const slug = webmention["wm-target"]
+      .replace("https://sebastiandedeyne.com/", "")
+      .replace(/\/$/, "")
+      .replace("/", "--");
 
-  if (fs.lstatSync(fullPath).isDirectory()) {
-    return fs
-      .readdirSync(fullPath)
-      .map(subPath => `${path}--${subPath.replace(".md", "")}`);
-  }
+    const filename = `${__dirname}/data/webmentions/${slug}.json`;
 
-  return [path.replace(".md", "")];
+    // If there's no data file for the webmention target yet, we can simply
+    // create a new one with the webmention as the sole entry.
+    if (!fs.existsSync(filename)) {
+      fs.writeFileSync(filename, JSON.stringify([webmention], null, 2));
+
+      return;
+    }
+
+    // If there are already entries for a webmention target, append the
+    // new webmention to the existing ones. But first filter the existing
+    // entries to prevent duplicates.
+    const entries = JSON.parse(fs.readFileSync(filename))
+      .filter(wm => wm["wm-id"] !== webmention["wm-id"])
+      .concat([webmention]);
+
+    // Sort the entries by webmention ID to have a deterministic order.
+    entries.sort((a, b) => a["wm-id"] - b["wm-id"]);
+
+    fs.writeFileSync(filename, JSON.stringify(entries, null, 2));
+  });
 });
-
-fetchWebmentions().then(webmentionsResponse => {
-  if (!("children" in webmentionsResponse)) {
-    throw new Error("Invalid webmention.io response.");
-  }
-
-  webmentionsResponse.children
-    .filter(entry => postSlugs.includes(getSlug(entry)))
-    .forEach(entry => {
-      const filename = `${__dirname}/data/webmentions/${getSlug(entry)}.json`;
-
-      if (!fs.existsSync(filename)) {
-        fs.writeFileSync(filename, JSON.stringify([entry], null, 2));
-        return;
-      }
-
-      const entries = JSON.parse(fs.readFileSync(filename));
-
-      const newEntries = uniqBy([entry, ...entries], entry => entry["wm-id"]);
-      newEntries.sort((a, b) => a["wm-id"] - b["wm-id"]);
-
-      fs.writeFileSync(filename, JSON.stringify(newEntries, null, 2));
-    });
-});
-
-function getSlug(entry) {
-  return entry["wm-target"]
-    .replace("https://sebastiandedeyne.com/", "")
-    .replace(/\/$/, "")
-    .replace("/", "--");
-}
 
 function fetchWebmentions() {
+  // Token will be passed as an environment variable by Netlify.
   const token = process.env.WEBMENTIONS_TOKEN;
 
+  // Fetch all webmentions from the past 3 days.
   const since = new Date();
-  since.setDate(since.getDate() - 1);
+  since.setDate(since.getDate() - 3);
 
   const url =
     "https://webmention.io/api/mentions.jf2" +
@@ -77,21 +70,11 @@ function fetchWebmentions() {
       .on("error", error => {
         reject(error);
       });
+  }).then(response => {
+    if (!("children" in response)) {
+      throw new Error("Invalid webmention.io response.");
+    }
+
+    return response.children;
   });
-}
-
-function uniqBy(array, callback) {
-  return Array.from(
-    array
-      .reduce((map, item) => {
-        const key = callback(item);
-
-        if (!map.has(key)) {
-          map.set(key, item);
-        }
-
-        return map;
-      }, new Map())
-      .values()
-  );
 }
